cd94460ecc188321fedbf4589f57f51cbaebd5e1
viewprox86 <workmates.live@outlook.com>
2024-01-04 06:11:13 -0600

    sdf

---
+++ src/app/health.js
@@ -0,0 +1,27 @@
+import DojosSection3 from "./components/DojosSection3";
+import Navbar from "./components/Navbar";
+
+
+
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div class="container mt-24 mx-auto px-12 py-4">
+        <DojosSection3 />
+      </div>
+    </main>
+      
+  
+  );
+
+
+}
+
+
+
+
+
