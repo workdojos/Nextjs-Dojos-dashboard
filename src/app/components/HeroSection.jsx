7fda638e212fd03f585c0cc2184e3b26a7b9b057
viewprox86 <workmates.live@outlook.com>
2024-02-01 09:17:30 -0600

    sd

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -44,7 +44,7 @@ const HeroSection = () => {
               href="https://blog.workdojos.com/#/portal/signup"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Join for Free
+              Join for free
             </Link>
             
           </div>
