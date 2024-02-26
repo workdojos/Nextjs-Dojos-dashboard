d672699f1971c0baf59bd0664af4a8c42f14b4e5
workdojos <itsarectangle@gmail.com>
2024-02-26 11:02:53 -0600

    as

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -40,6 +40,15 @@ const HeroSection = () => {
             Take the 60-day self-hosting challenge and behold the many wonders of open-source technology.
           </p>
           <div>
+            <br></br>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            The dashboard is just the beginning.  A gateway to all your self-hosted apps.
+          </p>
+          <br></br>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Your server.  Your data.  You are in control.
+          </p>
+          <div>
             <Link
               href="https://blog.workdojos.com/#/portal/signup"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
@@ -67,7 +76,38 @@ const HeroSection = () => {
           </div>
         </motion.div>
       </div>
+      
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              {" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Your Server",
+                1000,
+                "Your Data",
+                1000,
+                "You're in Control",
+                1000,
+
+                
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+      </div>
     </section>
+    
   );
 };
 
