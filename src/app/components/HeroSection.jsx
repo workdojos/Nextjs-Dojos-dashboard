0df646306883c5c7ba1aef167e4e07b57fb1031c
workdojos <itsarectangle@gmail.com>
2024-02-26 13:04:00 -0600

    sd

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -36,7 +36,7 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mr-10 mb-6 lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Take the 60-day self-hosting challenge and behold the many wonders of open-source technology.  An ad-free space for bookmarks and links to your own apps parked at your own domain.  And the dashboard is just the beginning!
           </p>
           <div>
