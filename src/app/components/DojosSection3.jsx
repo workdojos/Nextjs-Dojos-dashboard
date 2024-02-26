18f12402bd00085dd455472a6faea6d46ab11a53
workdojos <itsarectangle@gmail.com>
2024-02-26 10:11:28 -0600

    as

--- src/app/components/DojosSection3.jsx
+++ src/app/components/DojosSection3.jsx
@@ -11,7 +11,7 @@ const dojosData = [
     description: "Use open-source sensor technology to create a smart greenhouse system that monitors temperature, humidity, and light levels, providing real-time data and automated adjustments for optimal cannabis growth, maximizing yield and reducing costs.",
     image: "/images/projects/growers.webp",
     tag: ["All", "Health & Wellness"],
-    gitUrl: "https://og.admins.live/#Growers",
+    gitUrl: "Growers",
     previewUrl: "/",
   },
   {
@@ -20,7 +20,7 @@ const dojosData = [
     description: "Use open-source telemedicine platforms to enable doctors to provide remote consultations and collaborate with colleagues globally, improving accessibility to healthcare and facilitating knowledge sharing for better patient outcomes.",
     image: "/images/projects/clinician.webp",
     tag: ["All", "Health & Wellness"],
-    gitUrl: "https://og.admins.live/#Doctors",
+    gitUrl: "Doctors",
     previewUrl: "/",
   },
   {
@@ -29,7 +29,7 @@ const dojosData = [
     description: "Open-source technology can be used by explorers to create a collaborative mapping platform that allows individuals to share real-time discoveries and navigate uncharted territories, fostering global exploration and discovery.",
     image: "/images/projects/explorers.webp",
     tag: ["All", "Travel"],
-    gitUrl: "https://og.admins.live/#Explorers",
+    gitUrl: "Explorers",
     previewUrl: "/",
   },
   {
@@ -38,7 +38,7 @@ const dojosData = [
     description: "Open-source technology can be utilized by life coaches to develop a collaborative online platform that offers personalized goal tracking and interactive coaching sessions, fostering client engagement and promoting self-improvement.",
     image: "/images/projects/lifecoaches.webp",
     tag: ["All", "Health & Wellness"],
-    gitUrl: "https://og.admins.live/#Life-Coaches",
+    gitUrl: "Life-Coaches",
     previewUrl: "/",
   },
   {
@@ -47,7 +47,7 @@ const dojosData = [
     description: "Neighborhood volunteers can use open-source technology to create a community-driven platform for sharing resources and organizing events, fostering collaboration and empowerment within the local community.",
     image: "images/projects/neighborly.webp",
     tag: ["All", "Public Services"],
-    gitUrl: "https://og.admins.live/#Neighbors",
+    gitUrl: "Neighbors",
     previewUrl: "/",
   },
   {
@@ -56,7 +56,7 @@ const dojosData = [
     description: "Open-source technology can be used by park rangers to develop a mobile app that enables visitors to identify local flora and fauna, fostering environmental awareness and engagement.",
     image: "/images/projects/parkranger.webp",
     tag: ["All", "Public Services"],
-    gitUrl: "https://og.admins.live/#Park-Rangers",
+    gitUrl: "Park-Rangers",
     previewUrl: "/",
   },
   {
@@ -65,7 +65,7 @@ const dojosData = [
     description: "Open-source technology can be used by pharmacists to develop a collaborative medication management platform that enables real-time sharing of drug interactions and personalized treatment recommendations, improving patient safety and healthcare outcomes.",
     image: "/images/projects/pharmacists.webp",
     tag: ["All", "Health & Wellness"],
-    gitUrl: "https://og.admins.live/#Pharmacists",
+    gitUrl: "Pharmacists",
     previewUrl: "/images/projects/academies.jpg",
   },
   {
@@ -74,7 +74,7 @@ const dojosData = [
     description: "Open-source technology can be used by Registered Nurses to develop a collaborative mobile app that enables real-time communication and information sharing among healthcare professionals, improving patient care coordination and efficiency.",
     image: "/images/projects/physicaltherapist.webp",
     tag: ["All", "Health & Wellness"],
-    gitUrl: "https://og.admins.live/#Physical-Therapists",
+    gitUrl: "Physical-Therapists",
     previewUrl: "/",
   },
   {
@@ -83,7 +83,7 @@ const dojosData = [
     description: "Open-source project management software provides collaborative task tracking and real-time updates, empowering project managers to efficiently coordinate teams and streamline project workflows.",
     image: "/images/projects/registerednurse.webp",
     tag: ["All", "Health & Wellness"],
-    gitUrl: "https://og.admins.live/#Nurses",
+    gitUrl: "Nurses",
     previewUrl: "/",
   },
   {
@@ -92,7 +92,7 @@ const dojosData = [
     description: "Open-source technology can be used by social workers to develop a collaborative platform for sharing resources, case studies, and best practices, fostering a global community of practitioners. This platform could also incorporate AI-powered analytics to identify trends and patterns, enabling data-driven decision-making and improving social interventions. By leveraging open-source technology, social workers can enhance their impact and create a more connected and effective support system for vulnerable populations.",
     image: "/images/projects/socialworker.webp",
     tag: ["All", "Public Services"],
-    gitUrl: "https://og.admins.live/#Social-Workers",
+    gitUrl: "Social-Workers",
     previewUrl: "/",
   },
   {
@@ -101,7 +101,7 @@ const dojosData = [
     description: "Use open-source technology to build a travel itinerary generator that suggests personalized destinations, accommodations, and activities based on user preferences, leveraging crowd-sourced reviews and real-time data. This tool can streamline the travel planning process, enhance customer satisfaction, and enable Travel Agents to offer unique and tailored experiences to their clients.",
     image: "images/projects/travelagents.webp",
     tag: ["All", "Travel"],
-    gitUrl: "https://og.admins.live/#Travel-Agents",
+    gitUrl: "Travel-Agents",
     previewUrl: "/",
   },
   {
@@ -110,7 +110,7 @@ const dojosData = [
     description: "Open-source technology can be used by Yoga Instructors to develop a customizable virtual reality (VR) yoga experience, allowing students to practice in immersive virtual environments with real-time feedback and guidance, enhancing their yoga journey and accessibility to the practice.",
     image: "/images/projects/yogainstructor.webp",
     tag: ["All", "Health & Wellness"],
-    gitUrl: "https://og.admins.live/#Yoga-Instructors",
+    gitUrl: "Yoga-Instructors",
     previewUrl: "/",
   },
 ];
