4fffea9e9b58eb8a8de417ed1f0de96b0e21e40a
workdojos <itsarectangle@gmail.com>
2024-04-20 07:48:15 -0500

    sd

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -57,7 +57,7 @@ const HeroSection = () => {
               href="https://workmates.live/marketplace"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
-              Browse All
+              Browse
             </Link>
             
           </div>
